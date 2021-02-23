b0b45e35b10f9b731907dda54aea46295e166522
Lucas Santos <lhs.santoss@gmail.com>
2021-02-23 15:18:19 -0300

    Change env name
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-export GENERAL_NAME=learn-aks-scalability
+export RESOURCE_NAME=learn-aks-scalability
 
 echo "Creating cluster..."
 
-az group create $GENERAL_NAME -l eastus
+az group create $RESOURCE_NAME -l eastus
 
-az aks create -g $GENERAL_NAME -n $GENERAL_NAME --node-count=1 --generate-ssh-keys --enable-addons http_application_rounting --vm-size Standard_B2s
+az aks create -g $RESOURCE_NAME -n $RESOURCE_NAME --node-count=1 --generate-ssh-keys --enable-addons http_application_rounting --vm-size Standard_B2s
 
-az aks get-credentials -g $GENERAL_NAME -n $GENERAL_NAME
+az aks get-credentials -g $RESOURCE_NAME -n $RESOURCE_NAME
 
 echo "Gathering DNS name"
 
-export DNS_NAME=$(az aks show -g $GENERAL_NAME -n $GENERAL_NAME -o tsv --query addonProfiles.httpApplicationRouting.config.HTTPApplicationRoutingZoneName)
+export DNS_NAME=$(az aks show -g $RESOURCE_NAME -n $RESOURCE_NAME -o tsv --query addonProfiles.httpApplicationRouting.config.HTTPApplicationRoutingZoneName)
 sed -i '' 's+!DNS!+'"$DNS_NAME"'+g' ./ingress.yaml
 
 kubectl apply -f .
