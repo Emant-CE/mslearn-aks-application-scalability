7d9909ab482a8a8dd2a38b3212497ef105cfddac
Lucas Santos <lhs.santoss@gmail.com>
2021-02-23 15:33:11 -0300

    Correct remote references
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -6,15 +6,14 @@ echo "Creating cluster..."
 
 az group create -n $RESOURCE_NAME -l eastus
 
-az aks create -g $RESOURCE_NAME -n $RESOURCE_NAME --node-count=1 --generate-ssh-keys --enable-addons http_application_rounting --vm-size Standard_B2s
+az aks create -g $RESOURCE_NAME -n $RESOURCE_NAME --node-count=1 --generate-ssh-keys --enable-addons http_application_rounting -s Standard_B2s
 
 az aks get-credentials -g $RESOURCE_NAME -n $RESOURCE_NAME
 
 echo "Gathering DNS name"
 
 DNS_NAME=$(az aks show -g $RESOURCE_NAME -n $RESOURCE_NAME -o tsv --query addonProfiles.httpApplicationRouting.config.HTTPApplicationRoutingZoneName)
-sed -i '' 's+!DNS!+'"$DNS_NAME"'+g' ./ingress.yaml
 
 kubectl apply -f https://raw.githubusercontent.com/Azure-Samples/mslearn-aks-application-scalability/main/kubernetes/deployment.yaml
-kubectl apply -f https://raw.githubusercontent.com/Azure-Samples/mslearn-aks-application-scalability/main/kubernetes/ingress.yaml
+curl -L https://raw.githubusercontent.com/Azure-Samples/mslearn-aks-application-scalability/main/kubernetes/ingress.yaml | sed 's+!DNS!+'"$DNS_NAME"'+g' | kubectl apply -f -
 kubectl apply -f https://raw.githubusercontent.com/Azure-Samples/mslearn-aks-application-scalability/main/kubernetes/service.yaml
